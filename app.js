8267988419d417b76602dbe7d0f012becbf2bf8d
dnikic <danilonikic4@gmail.com>
2020-05-04 00:28:26 +0200

    Transfered media app to websocket base

--- app.js
+++ app.js
@@ -2,29 +2,82 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var connected_num = 0
 
-app.get('/', function(req, res) {
-   res.sendfile('index.html');
+
+app.get('/', function (req, res) {
+   // res.sendfile('capture.html');
+   res.sendFile('index.html', {root: __dirname});
 });
 
-io.on('connection', function(socket) {
+app.get('/capture', function (req, res) {
+   // res.sendfile('capture.html');
+   res.sendFile('capture.html', {root: __dirname});
+});
+app.get('/capture.js', function (req, res) {
+   // res.sendfile('capture.js');
+   res.sendFile('capture.js', {root: __dirname});
+});
+app.get('/audio/1.wav', function (req, res) {
+   // res.sendfile('audio/1.wav');
+   res.sendFile('audio/1.wav', {root: __dirname});
+});
+app.get('/audio/2.wav', function (req, res) {
+   // res.sendfile('audio/2.wav');
+   res.sendFile('audio/2.wav', {root: __dirname});
+});
+
+
+
+io.on('connection', function (socket) {
    console.log('A user connected');
+   connected_num = connected_num + 1
 
    //Send a message when 
-   setTimeout(function() {
-      //Sending an object when emmiting an event
-      socket.emit('testerEvent', { description: 'A custom event named testerEvent!'});
-   }, 4000);
+   // setTimeout(function () {
+   //    //Sending an object when emmiting an event
+   //    socket.emit('testerEvent', { description: 'A custom event named testerEvent!' });
+   // }, 4000);
 
-   socket.on('clientEvent', function(data) {
+   socket.on('clientEvent', function (data) {
       console.log(data);
    });
 
    socket.on('disconnect', function () {
       console.log('A user disconnected');
+      // Shutdown server if no connected clients
+      connected_num = connected_num - 1
+      if (connected_num == 0) {   
+         http.close()   
+         io.close()   
+         process.exit();
+      }
    });
+
 });
 
-http.listen(3000, function() {
+http.listen(3000, function () {
    console.log('listening on localhost:3000');
 });
+
+
+
+// Start Firefox
+try {
+   const { exec } = require("child_process");
+   exec("firefox -new-tab 'localhost:3000/capture'", (error, stdout, stderr) => {
+      if (error) {
+         console.log(`error: ${error.message}`);
+         return;
+      }
+   });
+   exec("start firefox -new-tab localhost:3000/capture", (error, stdout, stderr) => {
+      if (error) {
+         console.log(`error: ${error.message}`);
+         return;
+      }
+   });
+}
+catch (e) {
+   console.log('Firefoxx not available.')
+}
---
+++ capture.js
@@ -0,0 +1,115 @@
+// console.log('Debug________________________');
+
+//Sockets
+var socket = io();
+socket.on('testerEvent', function (data) {
+    console.log(data.description);
+    document.write(data.description)
+});
+
+socket.emit('clientEvent', 'Sent an event from the client!');
+
+
+
+// AR
+window.addEventListener('camera-init', (data) => {
+    console.log('camera-init', data);
+})
+
+window.addEventListener('camera-error', (error) => {
+    console.log('camera-error', error);
+})
+
+AFRAME.registerComponent('registerevents', {
+
+    init: function () {
+        var marker = this.el;
+        var interval;
+
+        marker.addEventListener('markerFound', function () {
+            var markerId = marker.id;
+            console.log('markerFound', markerId);
+            // TODO: Add your own code here to react to the marker being found.
+            if (markerId == 'marker-hiro') { sound1.pause(); }
+            if (markerId == 'marker-kanji') { sound2.pause();}
+            // Start interval loop
+            var i = 0;
+            var amount = 0
+            interval = setInterval(function () {
+                // console.log(i++);  // this is inside your loop
+                position = marker.getAttribute('position');
+                rotation = marker.getAttribute('rotation');
+                amount = Math.abs(position.x)
+                if (amount > 1.5) {
+                    amount = 5
+                }
+                else {
+                    amount = 0.2
+                }
+
+                console.log(amount)
+                changeAudio(sound1, amount, amount, amount)
+                if (markerId == 'marker-hiro') { sound1.play(); }
+                if (markerId == 'marker-kanji') { sound2.play();}
+            }, 500);
+
+        });
+
+        marker.addEventListener('markerLost', function () {
+            var markerId = marker.id;
+            console.log('markerLost', markerId);
+            // TODO: Add your own code here to react to the marker being lost.
+            // sound1.stop();
+            if (markerId == 'marker-hiro') { sound1.pause(); }
+            if (markerId == 'marker-kanji') { sound2.pause();}
+            clearInterval(interval);
+        });
+    }
+});
+
+// Audio
+var sound1 = new Pizzicato.Sound(
+    {
+        source: 'file',
+        options: { path: './audio/1.wav', loop: true }
+    },
+    function () {
+        // Sound loaded!
+        console.log('Sound loaded')
+        // sound1.play();
+    });
+var sound2 = new Pizzicato.Sound(
+    {
+        source: 'file',
+        options: { path: './audio/2.wav', loop: true }
+    },
+    function () {
+        // Sound loaded!
+        console.log('Sound loaded')
+        // sound1.play();
+    });
+
+
+
+function changeAudio(sound1, revv, distv, volv) {
+
+    sound1.removeEffect(distortion)
+    sound1.removeEffect(reverb)
+    var reverb = new Pizzicato.Effects.Reverb({
+        time: 0.90,
+        decay: 0.03,
+        reverse: false,
+        mix: revv//max 0.71, min 0.00
+    });
+    var distortion = new Pizzicato.Effects.Distortion({
+        gain: distv //min 0.0, max 1.0
+    });
+    // sound1.addEffect(distortion);
+    // sound1.addEffect(reverb);
+
+    sound1.volume = volv;
+    return sound1
+}
+
+
+
